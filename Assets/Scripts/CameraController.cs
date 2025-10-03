64a2ce8860328434dd6fdca7fe070f04686b7b56
runaarai <luna.alay1006@gmail.com>
2025-10-03 21:08:14 +0900

    CameraController追加。プレイヤーを追従

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,14 +2,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    GameObject player;
+    Vector3 diff;//ターゲットとの距離の差
+    public float followSpeed = 5.0f; //カメラの補間スピード
 
     void Start()
     {
-        
+        //プレイヤー情報を取得
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //スタートした瞬間のカメラの現在地
+        transform.position = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y,
+            -10
+        );
+
+        //プレイヤーとカメラの距離感を記憶しておく
+        diff = player.transform.position - transform.position;
     }
 
-    void Update()
+    void LateUpdate()
     {
-        
+        //プレイヤーが見つからない場合は何もしない
+        if (player == null) return;
+            //線形補間を使って、カメラを目的の場所に動かす
+            //Lerpメソッド(今の位置、ゴールとすべき位置、割合）
+            transform.position = Vector3.Lerp(transform.position, player.transform.position - diff, followSpeed * Time.deltaTime);
     }
 }
