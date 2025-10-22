03bfd60304ad291eb65104588c2a24a54e9f02a8
runaarai <luna.alay1006@gmail.com>
2025-10-22 22:30:37 +0900

    消化器を一度使用すると所持数から-2される問題解決。（異なるスクリプトで同じコード「GameManager.Extinguisher--;」を入れていた）

--- Assets/Scripts/ExtinguisherController.cs
+++ Assets/Scripts/ExtinguisherController.cs
@@ -14,7 +14,6 @@ public class ExtinguisherController : MonoBehaviour
     {
         Instantiate(smokePrefab, transform.position, Quaternion.identity); //消化器と同じ場所にバリア生成
         Destroy(gameObject);  //消化器は消滅
-        GameManager.Extinguisher--;//所持アイテム数から１減らす
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
