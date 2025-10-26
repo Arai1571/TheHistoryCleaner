52116367e1f0a3afd06d6bbbdd8403b3431483a6
runaarai <luna.alay1006@gmail.com>
2025-10-26 18:48:50 +0900

    タイトルシーンボタン変更

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -3,11 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
-    public void StartGame()
+    public void NewGame()
     {
         //ゲームの初期化
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene("Opening");
     }
 
+    public void ReStart()
+    {
+        //ゲームの初期化
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Main");
+    }
 }
